ce1f8466915038ff88152bba62733c3b557838c7
Raaghav Karthik <raaghav.juju@gmail.com>
2017-03-20 11:37:34 -0400

    fixed prev error: search feature for matches

--- Android/Gym-Me/app/src/main/java/com/example/juju/gym_me/ViewMatchFragment.java
+++ Android/Gym-Me/app/src/main/java/com/example/juju/gym_me/ViewMatchFragment.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.widget.SearchView;
+import android.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
