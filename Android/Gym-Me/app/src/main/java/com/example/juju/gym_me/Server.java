85cc776f9641984553143161d597d10410b69e50
goelm <goelm@purdue.edu>
2017-04-25 21:01:17 -0400

    server stuff for goals

--- Android/Gym-Me/app/src/main/java/com/example/juju/gym_me/Server.java
+++ Android/Gym-Me/app/src/main/java/com/example/juju/gym_me/Server.java
@@ -59,6 +59,10 @@ public class Server extends AsyncTask<String,String,String> {
                 unblock: second is blocker, third is blockee
                 getallblocked: 2-blocker
                 reportabuse: 2 - reporter, 3 - abuser
+                createmonthlygoal: 2 - email, 3 - currentgoal, 4 - completegoal
+                editmonthlygoal: 2 - email, 3 - currentgoal, 4 - completegoal
+                getmonthlygoal: 2-email
+
 
             return values:
                 register: "success", "unsuccessful" if response was not OK, or an error message from backend
@@ -866,7 +870,6 @@ public class Server extends AsyncTask<String,String,String> {
 
         }
 
-        //TODO
         if (params[0] == "sortbytag") {
 
             try {
@@ -1123,7 +1126,7 @@ public class Server extends AsyncTask<String,String,String> {
 
             try {
 
-                URL url = new URL("http://10.0.2.2:8080/addschedule");
+                URL url = new URL("http://10.0.2.2:8080/createsharedschedule");
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
 
                 JSONObject jsonParam = new JSONObject();
@@ -1534,6 +1537,266 @@ public class Server extends AsyncTask<String,String,String> {
                     }
                     else {
                         return ("success");
+                    }
+                } else {
+
+                    return ("unsuccessful");
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        if (params[0] == "sortbyname") {
+
+            try {
+
+                URL url = new URL("http://10.0.2.2:8080/sortbyname");
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+                //getallprofiles: 2-email, 3-password
+                JSONObject jsonParam = new JSONObject();
+                jsonParam.put("email", params[1]);
+                jsonParam.put("password", hashPassword(params[2]));
+                jsonParam.put("tag", params[3]);
+
+                urlConnection.setDoOutput(true);
+                urlConnection.setRequestProperty("Content-Type", "application/json");
+                urlConnection.setRequestMethod("POST");
+                urlConnection.connect();
+
+                DataOutputStream wr = new DataOutputStream(urlConnection.getOutputStream());
+                wr.writeBytes(jsonParam.toString());
+                wr.flush();
+                wr.close();
+
+                int response_code = urlConnection.getResponseCode();
+
+                // Check if successful connection made
+                if (response_code == HttpURLConnection.HTTP_OK) {
+
+                    // Read data sent from server
+                    InputStream input = urlConnection.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                    StringBuilder result = new StringBuilder();
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        result.append(line);
+                    }
+
+                    // Parse JSON object and return it
+                    JSONObject obj = new JSONObject(result.toString());
+                    if(obj.getString("success").equals("false")){
+                        return (obj.getString("errormsg").toString());
+                    }
+                    else {
+                        JSONArray arr = obj.getJSONArray("users");
+                        int length = arr.length();
+                        if(length == 0){
+                            return "empty";
+                        }
+                        String usernames_list = "";
+                        for(int i = 0; i<length; i++){
+                            String username = arr.getJSONObject(i).getString("user_email");
+                            usernames_list = usernames_list + username + ",";
+                        }
+                        return usernames_list;
+                    }
+                } else {
+
+                    return ("unsuccessful");
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        if (params[0] == "createmonthlygoal") {
+
+            try {
+
+                URL url = new URL("http://10.0.2.2:8080/createmonthlygoal");
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+                //createmonthlygoal: 2 - email, 3 - currentgoal, 4 - completegoal
+                JSONObject jsonParam = new JSONObject();
+                jsonParam.put("email", params[1]);
+                jsonParam.put("currentgoal", params[2]);
+                jsonParam.put("completegoal", params[3]);
+
+                urlConnection.setDoOutput(true);
+                urlConnection.setRequestProperty("Content-Type", "application/json");
+                urlConnection.setRequestMethod("POST");
+                urlConnection.connect();
+
+                DataOutputStream wr = new DataOutputStream(urlConnection.getOutputStream());
+                wr.writeBytes(jsonParam.toString());
+                wr.flush();
+                wr.close();
+
+                int response_code = urlConnection.getResponseCode();
+
+                // Check if successful connection made
+                if (response_code == HttpURLConnection.HTTP_OK) {
+
+                    // Read data sent from server
+                    InputStream input = urlConnection.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                    StringBuilder result = new StringBuilder();
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        result.append(line);
+                    }
+
+                    // Parse JSON object and return it
+                    JSONObject obj = new JSONObject(result.toString());
+                    if(obj.getString("success").equals("false")){
+                        return (obj.getString("errormsg").toString());
+                    }
+                    else {
+                        return ("success");
+                    }
+                } else {
+
+                    return ("unsuccessful");
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        if (params[0] == "editmonthlygoal") {
+
+            try {
+
+                URL url = new URL("http://10.0.2.2:8080/editmonthlygoal");
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+                //editmonthlygoal: 2 - email, 3 - currentgoal, 4 - completegoal
+                JSONObject jsonParam = new JSONObject();
+                jsonParam.put("email", params[1]);
+                jsonParam.put("currentgoal", params[2]);
+                jsonParam.put("completegoal", params[3]);
+
+                urlConnection.setDoOutput(true);
+                urlConnection.setRequestProperty("Content-Type", "application/json");
+                urlConnection.setRequestMethod("POST");
+                urlConnection.connect();
+
+                DataOutputStream wr = new DataOutputStream(urlConnection.getOutputStream());
+                wr.writeBytes(jsonParam.toString());
+                wr.flush();
+                wr.close();
+
+                int response_code = urlConnection.getResponseCode();
+
+                // Check if successful connection made
+                if (response_code == HttpURLConnection.HTTP_OK) {
+
+                    // Read data sent from server
+                    InputStream input = urlConnection.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                    StringBuilder result = new StringBuilder();
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        result.append(line);
+                    }
+
+                    // Parse JSON object and return it
+                    JSONObject obj = new JSONObject(result.toString());
+                    if(obj.getString("success").equals("false")){
+                        return (obj.getString("errormsg").toString());
+                    }
+                    else {
+                        return ("success");
+                    }
+                } else {
+
+                    return ("unsuccessful");
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        if (params[0] == "getmonthlygoal") {
+
+            try {
+
+                URL url = new URL("http://10.0.2.2:8080/getmonthlygoal");
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+                //getmonthlygoal: 2-email
+                JSONObject jsonParam = new JSONObject();
+                jsonParam.put("email", params[1]);
+
+                urlConnection.setDoOutput(true);
+                urlConnection.setRequestProperty("Content-Type", "application/json");
+                urlConnection.setRequestMethod("POST");
+                urlConnection.connect();
+
+                DataOutputStream wr = new DataOutputStream(urlConnection.getOutputStream());
+                wr.writeBytes(jsonParam.toString());
+                wr.flush();
+                wr.close();
+
+                int response_code = urlConnection.getResponseCode();
+
+                // Check if successful connection made
+                if (response_code == HttpURLConnection.HTTP_OK) {
+
+                    // Read data sent from server
+                    InputStream input = urlConnection.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                    StringBuilder result = new StringBuilder();
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        result.append(line);
+                    }
+
+                    // Parse JSON object and return it
+                    JSONObject obj = new JSONObject(result.toString());
+                    if(obj.getString("success").equals("false")){
+                        return (obj.getString("errormsg").toString());
+                    }
+                    else {
+                        JSONArray arr = obj.getJSONArray("schedule");
+                        int length = arr.length();
+                        if(length == 0){
+                            return "empty";
+                        }
+                        String goals = arr.getJSONObject(0).getString("current");
+                        goals += ";;;";
+                        goals += arr.getJSONObject(0).getString("finished");
+                        return goals;
                     }
                 } else {
 
